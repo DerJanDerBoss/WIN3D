31cc0ccb64781603a603524ccb7f8be7c03bcdba
giomen <giomen@eg-ts02.einhard.intern>
2018-06-18 12:19:34 +0200

    Rotate Vector Method

--- WIN3D/src/classes/Vector.java
+++ WIN3D/src/classes/Vector.java
@@ -1,9 +1,19 @@
 package classes;
+
 public class Vector {
-public double x;
-public double y;
-public Vector(double pX, double pY){
-	x = pX;
-	y = pY;
-}
+	public double x;
+	public double y;
+	public Vector(double pX, double pY){
+		x = pX;
+		y = pY;
+	}
+	public static Vector rotateVector(Vector vector,  int degrees)
+	{
+		double x = vector.x;
+		double y = vector.y;
+		x -= Math.cos(degrees / 360 * 2 * Math.PI);
+		y += Math.sin(degrees / 360 * 2 * Math.PI);
+		return new Vector(x, y);
+	}
+
 }
