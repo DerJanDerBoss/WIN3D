477414ede7dda5d691366d926df1a175d93a573f
Jan <Jan@JanPC.fritz.box>
2018-06-18 23:36:22 +0200

    window

--- WIN3D/src/GUI/FirstWindow.java
+++ WIN3D/src/GUI/FirstWindow.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import java.awt.Color;
 
 public class FirstWindow extends JFrame {
 
@@ -36,11 +37,13 @@ public class FirstWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 961, 686);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.YELLOW);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		JLabel lblTestlabel = new JLabel("TestLabel17");
+		lblTestlabel.setBackground(new Color(255, 255, 0));
 		contentPane.add(lblTestlabel, BorderLayout.WEST);
 	}
 
