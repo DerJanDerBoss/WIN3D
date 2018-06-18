f27a217fb53ba2c23949397b15006611729dea05
maxole <maxole@eg-ts02.einhard.intern>
2018-06-18 12:31:03 +0200

    tastenabfrageVers1

--- WIN3D/src/classes/Player.java
+++ WIN3D/src/classes/Player.java
@@ -1,7 +1,10 @@
 package classes;
-public class Player extends GameObjects{
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Player extends GameObjects implements KeyListener{
 	public Player(){
-		//SAAAAAAAS
 	}
 
 	public void move(){
@@ -14,6 +17,35 @@ public class Player extends GameObjects{
 		
 	}
 	public void interact(){
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if(arg0.getKeyChar()=='w' || arg0.getKeyChar()=='W'){
+			
+		}
+		else if(arg0.getKeyChar()=='s' || arg0.getKeyChar()=='S'){
+			
+		}
+		else if(arg0.getKeyChar()=='a' || arg0.getKeyChar()=='A'){
+			
+		}
+		else if(arg0.getKeyChar()=='d' || arg0.getKeyChar()=='D'){
+			
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
