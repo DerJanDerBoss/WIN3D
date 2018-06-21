105c3de75a994ee96186536389a2daa9d317d4f1
maxole <maxole@eg-ts02.einhard.intern>
2018-06-21 10:18:37 +0200

    vectorfix

--- WIN3D/src/classes/Vector.java
+++ WIN3D/src/classes/Vector.java
@@ -1,4 +1,5 @@
 package classes;
+
 public class Vector {
 public double x;
 public double y;
@@ -12,4 +13,29 @@ public void setX(double pX){
 public void setY(double pY){
 	pY = y;
 }
+	public static Vector rotateVector(Vector vector,  int degrees)
+	{
+		double x = vector.x;
+		double y = vector.y;
+		x -= Math.cos(degrees / 360 * 2 * Math.PI);
+		y += Math.sin(degrees / 360 * 2 * Math.PI);
+		return new Vector(x, y);
+	}
+	public void add(Vector vector)
+	{
+		x += vector.x;
+		y += vector.y;
+	}
+	
+	public void subtract(Vector vector)
+	{
+		x -= vector.x;
+		y -= vector.y;
+	}
+	
+	public void multiply(double multiplier)
+	{
+		x *= multiplier;
+		y *= multiplier;
+	}
 }
