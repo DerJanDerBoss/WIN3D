e1eb21ae12fbc682d35d60cc2c9f198b49406059
giomen <giomen@eg-ts02.einhard.intern>
2018-06-25 08:14:44 +0200

    Renderer

--- WIN3D/src/classes/Renderer.java
+++ WIN3D/src/classes/Renderer.java
@@ -37,13 +37,30 @@ public class Renderer {
 			}
 		}
 		
-		for (int i = 0; i < screenWidth; i ++)
+		double[] renderedWallDistances = new double[screenWidth];
+ 		for (int i = 0; i < screenWidth; i ++)
 		{
 			double angle = Math.asin((i - (screenWidth / 2))/ screenWidth * 2 * Math.sin(fieldOfViewDegrees / 2 / 360 * Math.PI));
 			Vector ray = Vector.multiply(Vector.rotateVector(new Vector(Math.cos(angle),Math.sin(angle)), rotation), viewDistance);
-			for (int j = 0; j < relevantWalls.size(); i ++)
+			ArrayList<Double> wallIntersections = new ArrayList<Double>();
+			for (int j = 0; j < relevantWalls.size(); j ++)
 			{
-				
+				double[] intersection = Vector.checkForIntersections(position, ray, relevantWalls.get(j).getPos1(), Vector.subtract(relevantWalls.get(j).getPos2(), relevantWalls.get(j).getPos1()));
+				if (intersection[0] != 0 && intersection[1] >= 0 && intersection[2] >= 0 && intersection[2] >=1)
+				{
+					wallIntersections.add(intersection[1]);
+				}
+			}
+			if(wallIntersections.size() > 0){
+				double nearestIntersection = wallIntersections.get(0);
+				for (int j = 1; j < wallIntersections.size(); j ++)
+				{
+					if (wallIntersections.get(j) < nearestIntersection)
+					{
+						nearestIntersection = wallIntersections.get(j);
+					}
+				}
+				renderedWallDistances[i] = nearestIntersection;
 			}
 		}
 	}
--- WIN3D/src/classes/Vector.java
+++ WIN3D/src/classes/Vector.java
@@ -29,6 +29,54 @@ public class Vector {
 	{
 		return new Vector(vector.x * multiplier, vector.y * multiplier);
 	}
+	public static double[] checkForIntersections(Vector pos1, Vector dir1, Vector pos2, Vector dir2)
+	{
+		double[] returnValue = new double[3];
+		//check if directions are parralel and don't have a length of 0
+		if(dir1.x/dir2.x != dir1.y/dir2.y && dir1.getLength() != 0 && dir2.getLength() != 0)
+		{
+			if (dir1.x == 0)
+			{
+				returnValue[2] = (pos2.x - pos1.x) / - dir2.x;
+				if(dir2.y == 0)
+				{
+					returnValue[1] = (pos2.y - pos1.y) / dir1.y;
+				}
+				else
+				{
+					returnValue[1] = (pos2.y - pos1.y + dir2.y * returnValue[2]) / dir1.y;
+				}
+			}
+			else if(dir1.y == 0)
+			{
+				returnValue[2] = (pos2.y - pos1.y) / - dir2.y;
+				if(dir2.x == 0)
+				{
+					returnValue[1] = (pos2.x - pos1.x) / dir1.x;
+				}
+				else
+				{
+					returnValue[1] = (pos2.x - pos1.x + dir2.x * returnValue[2]) / dir1.x;
+				}
+			}
+			else if(dir2.x == 0)
+			{
+				returnValue[1] = (pos2.x - pos1.x) / dir1.x;
+				returnValue[2] = (pos2.y - pos1.y - dir1.y * returnValue[1]) / - dir2.y;
+			}
+			else if(dir2.y == 0)
+			{
+				returnValue[1] = (pos2.y - pos1.y) / dir1.y;
+				returnValue[2] = (pos2.x - pos1.x - dir1.x * returnValue[1]) / - dir2.x;
+			}
+			else
+			{
+				returnValue[1] = (pos2.y - pos2.x / (dir2.x / dir2.y)) / (dir1.y - dir1.x / (dir2.x / dir2.y));
+				returnValue[2] = (pos2.y - pos1.y - dir1.y * returnValue[1]) / - dir2.y;
+			}
+		}
+		return returnValue;
+	}
 	
 	public double getLength()
 	{
