d5a5e236117c841c1df9af66f3ccdcf5681716ed
giomen <giomen@eg-ts02.einhard.intern>
2018-06-25 08:14:44 +0200

    Renderer

--- WIN3D/src/classes/Vector.java
+++ WIN3D/src/classes/Vector.java
@@ -1,19 +1,13 @@
 package classes;
 
 public class Vector {
-public double x;
-public double y;
-public Vector(double pX, double pY){
-	x = pX;
-	y = pY;
-}
-public void setX(double pX){
-	pX = x;
-}
-public void setY(double pY){
-	pY = y;
-}
-	public static Vector rotateVector(Vector vector,  int degrees)
+	public double x;
+	public double y;
+	public Vector(double pX, double pY){
+		x = pX;
+		y = pY;
+	}
+	public static Vector rotateVector(Vector vector,  double degrees)
 	{
 		double x = vector.x;
 		double y = vector.y;
@@ -21,21 +15,71 @@ public void setY(double pY){
 		y += Math.sin(degrees / 360 * 2 * Math.PI);
 		return new Vector(x, y);
 	}
-	public void add(Vector vector)
+	public static Vector add(Vector vector1, Vector vector2)
 	{
-		x += vector.x;
-		y += vector.y;
+		return new Vector(vector1.x + vector2.x, vector1.y + vector2.y);
 	}
 	
-	public void subtract(Vector vector)
+	public static Vector subtract(Vector vector1, Vector vector2)
 	{
-		x -= vector.x;
-		y -= vector.y;
+		return new Vector(vector1.x - vector2.x, vector1.y - vector2.y);
 	}
 	
-	public void multiply(double multiplier)
+	public static Vector multiply(Vector vector, double multiplier)
 	{
-		x *= multiplier;
-		y *= multiplier;
+		return new Vector(vector.x * multiplier, vector.y * multiplier);
+	}
+	public static double[] checkForIntersections(Vector pos1, Vector dir1, Vector pos2, Vector dir2)
+	{
+		double[] returnValue = new double[3];
+		//check if directions are parralel and don't have a length of 0
+		if(dir1.x/dir2.x != dir1.y/dir2.y && dir1.getLength() != 0 && dir2.getLength() != 0)
+		{
+			if (dir1.x == 0)
+			{
+				returnValue[2] = (pos2.x - pos1.x) / - dir2.x;
+				if(dir2.y == 0)
+				{
+					returnValue[1] = (pos2.y - pos1.y) / dir1.y;
+				}
+				else
+				{
+					returnValue[1] = (pos2.y - pos1.y + dir2.y * returnValue[2]) / dir1.y;
+				}
+			}
+			else if(dir1.y == 0)
+			{
+				returnValue[2] = (pos2.y - pos1.y) / - dir2.y;
+				if(dir2.x == 0)
+				{
+					returnValue[1] = (pos2.x - pos1.x) / dir1.x;
+				}
+				else
+				{
+					returnValue[1] = (pos2.x - pos1.x + dir2.x * returnValue[2]) / dir1.x;
+				}
+			}
+			else if(dir2.x == 0)
+			{
+				returnValue[1] = (pos2.x - pos1.x) / dir1.x;
+				returnValue[2] = (pos2.y - pos1.y - dir1.y * returnValue[1]) / - dir2.y;
+			}
+			else if(dir2.y == 0)
+			{
+				returnValue[1] = (pos2.y - pos1.y) / dir1.y;
+				returnValue[2] = (pos2.x - pos1.x - dir1.x * returnValue[1]) / - dir2.x;
+			}
+			else
+			{
+				returnValue[1] = (pos2.y - pos2.x / (dir2.x / dir2.y)) / (dir1.y - dir1.x / (dir2.x / dir2.y));
+				returnValue[2] = (pos2.y - pos1.y - dir1.y * returnValue[1]) / - dir2.y;
+			}
+		}
+		return returnValue;
+	}
+	
+	public double getLength()
+	{
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 	}
 }
